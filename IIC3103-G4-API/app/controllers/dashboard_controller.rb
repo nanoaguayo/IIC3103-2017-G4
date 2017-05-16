25c0e3c4e2a4290bd0ae1299f7ad39928ed6330e
nanoaguayo <fdaguayo@uc.cl>
2017-05-15 20:39:32 -0400

    Dashboard connected to API

--- IIC3103-G4-API/app/controllers/dashboard_controller.rb
+++ IIC3103-G4-API/app/controllers/dashboard_controller.rb
@@ -1,59 +1,21 @@
 class DashboardController < ApplicationController
   def index
-    @almacenes = [{
-         "_id": "4af9f23d8ead0e1d32000000",
-         "usedSpace": 100,
-         "totalSpace": "10000",
-         "recepcion": false,
-         "despacho": false,
-         "pulmon": false,
-         "grupo": 4
-      },{
-         "_id": "4af9f23d8ead0e1d32000a00",
-         "usedSpace": 0,
-         "totalSpace": "99000",
-         "recepcion": true,
-         "despacho": false,
-         "pulmon": false,
-         "grupo": 4
-      },{
-         "_id": "4af9f23d8ead0e1d320f0a00",
-         "usedSpace": 0,
-         "totalSpace": "99999999",
-         "recepcion": false,
-         "despacho": false,
-         "pulmon": true,
-         "grupo": 4
-      },{
-         "_id": "4af9f24d8ead0e1d320f0a00",
-         "usedSpace": 60,
-         "totalSpace": "900",
-         "recepcion": false,
-         "despacho": true,
-         "pulmon": false,
-         "grupo": 4
-    }]
-    @productos = [{
-       "_id": {
-          "sku": 1
-        },
-        "total": 53
-    },{
-       "_id": {
-          "sku": 32
-        },
-        "total": 3
-    },{
-       "_id": {
-          "sku": 34
-        },
-        "total": 870
-    }]
+    @almacenes = Fetcher.Bodegas("GET","almacenes")
+    @productos = Hash.new 0
+    for almacen in @almacenes do
+      id = almacen['_id']
+      resp = Fetcher.Bodegas("GET"+id,"skusWithStock?almacenId="+id)
+      puts resp
+      for aux in resp do
+        @productos[aux['_id']] = @productos[aux['_id']] + aux['total']
+      end
+    end
+
     @row_data = Array.new(@productos.count)
     index = 0
-    for prod in @productos do
+    for prod in @productos.keys do
       puts prod
-      @row_data[index] = [prod[:_id][:sku],prod[:total]]
+      @row_data[index] = [prod,@productos[prod]]
       index = index +1
     end
   end
