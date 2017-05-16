77f773952bf8a92bb2b2660afaeace0d91ba5db0
Ricardo <rlira2@uc.cl>
2017-05-16 19:58:16 -0400

    Old feature/oc branch but updated with dev

--- IIC3103-G4-API/app/controllers/api/purchase_orders_controller.rb
+++ IIC3103-G4-API/app/controllers/api/purchase_orders_controller.rb
@@ -2,10 +2,17 @@ class Api::PurchaseOrdersController < Api::ApplicationController
     def create
       #TODO delete this
       @state = 'pending'
-      render :show
-      return 0
-      params_aux = check_params
-      @purchase_order = PurchaseOrder.create(params_aux)
+
+      path = "http://integracion-2017-dev.herokuapp.com/oc/obtener/" + params[:id]
+      puts path
+      header = {"Content-Type" => "application/json"}
+      @result = HTTParty.get(path, :query => {}, :header => header)
+      @ordenc = JSON.parse(@result.response.body)
+      #puts @ordenc.respond_to?body.fechaDespachos
+      #puts JSON.pretty_generate(@ordenc).gsub(":", " =>")
+      @purchase_order = PurchaseOrder.create(@ordenc)
+      render json: @result.response.body, status: :ok
+
       if @purchase_order.save then
         params[:id] = @purchase_order.id
         render :show, status: 202
@@ -19,7 +26,7 @@ class Api::PurchaseOrdersController < Api::ApplicationController
     def show
       @purchase_order = PurchaseOrder.find_by_id(params[:id])
       if @purchase_order || TRUE then
-          #render show
+          render :show
       else
           render_error('Order not found')
       end
