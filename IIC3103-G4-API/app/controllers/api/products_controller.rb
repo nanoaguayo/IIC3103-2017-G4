3517f7b269f3ba665ebe1ff464699289a76c4ff5
nanoaguayo <fdaguayo@uc.cl>
2017-05-17 00:33:05 -0400

    Fix call to update

--- IIC3103-G4-API/app/controllers/api/products_controller.rb
+++ IIC3103-G4-API/app/controllers/api/products_controller.rb
@@ -1,12 +1,12 @@
 class Api::ProductsController < Api::ApplicationController
 
   def index
+    updateStock
     @products = Product.all
   end
 
   def updateStock
     prods = Fetcher.getProductsWithStock
-    puts prods
     prods2 = Product.all
     for prod in prods2 do
       prod.stock = prods[prod.sku]
