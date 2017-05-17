4e2e73b0c231d2aa5ca61bf98e1ec91ae5ac78a8
nanoaguayo <fdaguayo@uc.cl>
2017-05-16 22:55:17 -0400

    Clean storage method for emptying storages

--- IIC3103-G4-API/app/controllers/api/ware_houses_controller.rb
+++ IIC3103-G4-API/app/controllers/api/ware_houses_controller.rb
@@ -39,15 +39,54 @@ class Api::WareHousesController < Api::ApplicationController
   end
 
 
-  def cleanPulmon
+  def cleanStorage
       #TODO
       almacenes = Fetcher.Bodegas("GET","almacenes")
+      id_pulmon = ""
+      id_recepcion = ""
+      otherStorages = Hash.new 0
       for alm in almacenes do
         if alm['pulmon'] && alm['usedSpace'] > 0 then
-          #Move to dispatch storage
-
+          id_pulmon = alm['_id']
+        elsif alm['recepcion'] && alm['usedSpace'] > 0 then
+          id_recepcion = alm['_id']
+        elsif !alm['recepcion'] && !alm['pulmon'] && !alm['despacho'] && (Integer(alm['totalSpace']) - Integer(alm['usedSpace'])>0) then
+          otherStorages[alm['_id']] = Integer(alm['totalSpace']) - Integer(alm['usedSpace'])
         end
       end
+      puts id_recepcion
+      puts id_pulmon
+      puts otherStorages
+      #Loop to clean all, first recepcion
+      prods = Fetcher.Bodegas("GET"+id_recepcion.to_s,"skusWithStock?almacenId="+id_recepcion.to_s)
+      for prod in prods do
+        #get products in storage for each sku
+        puts prod
+        prodList = Fetcher.Bodegas("GET"+id_recepcion.to_s+prod['_id'].to_s,"stock?almacenId="+id_recepcion.to_s+"&sku="+prod['_id']+"&limit=200")
+        puts prodList
+        count = 3
+        while prodList.count > 0 do
+          aux = prodList.pop
+          for key in otherStorages.keys do
+            if otherStorages[key]>0 then
+              moveStock(aux['_id'].to_s,key)
+              otherStorages[key] = otherStorages[key]-1
+              break
+            end
+          end
+          if count == 89 then
+              count = 0
+              sleep(60)
+          end
+          if prodList.count == 0 then
+              prodList = Fetcher.Bodegas("GET"+id_recepcion.to_s+prod['_id'].to_s,"stock?almacenId="+id_recepcion.to_s+"&sku="+prod['_id']+"&limit=200")
+              count = count + 1
+          end
+          count = count + 1
+        end
+      end
+      #Move to dispatch storage frm pulmon
+
   end
 
   #TODO check if working
