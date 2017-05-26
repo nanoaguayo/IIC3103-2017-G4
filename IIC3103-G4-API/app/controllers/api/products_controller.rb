6effeafd77ad8f898978cdb8f82da0c8ab1a3987
dasenRS <superdasen@gmail.com>
2017-05-26 12:55:48 -0400

    arreglé lo de recién, teniamos que mostrar solo nuestros productos

--- IIC3103-G4-API/app/controllers/api/products_controller.rb
+++ IIC3103-G4-API/app/controllers/api/products_controller.rb
@@ -15,10 +15,11 @@ class Api::ProductsController < Api::ApplicationController
   end
 
   def publico
+  	skus = SkuGroup.where(group: 4).map(&:sku)
   	updateStock
-  	products = Product.all
   	ret = Array.new
-  	products.each do |prod|
+  	skus.each do |sku|
+  		prod = Product.find_by(sku: sku)
   		ret << {sku: prod.sku, precio: prod.price, stock: prod.stock}
   	end
   	render json: ret
