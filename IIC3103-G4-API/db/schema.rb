69018928b39dc1ebb74e978074a0a31d100ac9f3
PDono19 <pjdonoso@uc.cl>
2017-06-26 23:15:45 -0400

    Insumos agregados

--- IIC3103-G4-API/db/schema.rb
+++ IIC3103-G4-API/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170531012838) do
+ActiveRecord::Schema.define(version: 20170620211419) do
 
   create_table "balances", force: :cascade do |t|
     t.string   "account"
